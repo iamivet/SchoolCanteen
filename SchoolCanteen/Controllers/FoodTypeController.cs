e5f0c9013d7cb5dbc188dce12ded3463baa87140
iamivet <121934923+iamivet@users.noreply.github.com>
2026-01-17 17:38:45 +0200

    Add method that takes all types of food in FOodTYpeController

--- SchoolCanteen/Controllers/FoodTypeController.cs
+++ SchoolCanteen/Controllers/FoodTypeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SchoolCanteen.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,20 @@ using System.Threading.Tasks;
 
 namespace SchoolCanteen.Controllers
 {
-    internal class FoodTypeController
+    public class FoodTypeController
     {
+        private SchoolCanteenDbContext dbContext;
+
+        public FoodTypeController()
+        {
+            dbContext = new SchoolCanteenDbContext();
+        }
+
+        public List<string> GetAllFoodTypes()
+        {
+            return dbContext.FoodTypes
+                .Select(f => f.Name)
+                .ToList();
+        }
     }
 }
